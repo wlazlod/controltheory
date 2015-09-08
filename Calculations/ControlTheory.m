8b471a2860617cb4c7970a9313b3cad663e92c45
Daniel Wlazło <wlazlo.daniel@gmail.com>
2015-09-08 21:59:49 +0200

    Added spectral and pseudo transfer functions.

--- Calculations/ControlTheory.m
+++ Calculations/ControlTheory.m
@@ -3,6 +3,13 @@ r1 = 1; %maximum speed
 r2 = 0.8; %maximum acceleration
 epsilon = 0.005; %accuracy
 
-%analysis of the transfer function G
+%analysis of the object and its discrete accurate transfer function 
+
 s=tf('s');  %definition of the operator s
 G=150/(s*(1.12*s+1)*(0.224*s+1)) %transfer function of object
+Tp=0.01; %sampling time, smaller than 1/10 of smallest time constant of G
+HG=c2d(G,Tp) %spectral transfer function
+HGW=d2c(HG,'tustin') %pseudo-transfer function
+HGWa=G*(1-0.5*Tp*s) %approximation of pseudo-transfer function
+
+%Bode plot for G, HG, HGW & HGWa
