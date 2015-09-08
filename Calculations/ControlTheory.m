4daf82b9785ce336b4690a29e0df0448ec3ebb79
Daniel Wlazło <wlazlo.daniel@gmail.com>
2015-09-08 22:15:49 +0200

    Added Body plots for transfer functions.

--- Calculations/ControlTheory.m
+++ Calculations/ControlTheory.m
@@ -1,9 +1,9 @@
-%data from the task
+%Data from the task
 r1 = 1; %maximum speed
 r2 = 0.8; %maximum acceleration
 epsilon = 0.005; %accuracy
 
-%analysis of the object and its discrete accurate transfer function 
+%Analysis of the object and its discrete accurate transfer function 
 
 s=tf('s');  %definition of the operator s
 G=150/(s*(1.12*s+1)*(0.224*s+1)) %transfer function of object
@@ -13,3 +13,13 @@ HGW=d2c(HG,'tustin') %pseudo-transfer function
 HGWa=G*(1-0.5*Tp*s) %approximation of pseudo-transfer function
 
 %Bode plot for G, HG, HGW & HGWa
+h=bodeplot(HGW, HGWa)
+p=getoptions(h);
+p.PhaseMatching='on';
+p.PhaseMatchingFreq=1.97;
+p.PhaseMatchingValue=-180;
+setoptions(h,p)
+hold
+bodeplot(G, HG)
+legend
+hold off;
